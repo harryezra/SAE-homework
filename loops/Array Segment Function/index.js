73d495c7b80e1817cd38cbcc3c52354ba1743127
Harry Levy <91187570+harryezra@users.noreply.github.com>
2022-03-03 18:26:59 +0100

    Delete loops directory

--- loops/Array Segment Function/index.js
+++
@@ -1,8 +0,0 @@
-var preArr = [1,2,3,4,5,6,7,8,9,10]
-
-function getData(arr.n){
-  return arr.splice(0,n)
-}
-
-var result = getData(preArr.3)
-console.log(result)
--- loops/spin151/main.js
+++
@@ -1,5 +0,0 @@
-for (var i - 1; i <- 151; i++){
-  if (i --- 151) {
-    console.log(i)
-  }
-}
